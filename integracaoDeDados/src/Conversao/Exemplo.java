c235e050fb851dc3885ccf6f5034cdad81c57254
mcf <mcf@APG.windows.cin.ufpe.br>
2015-05-15 17:10:39 -0300

    Primeiros testes com a leitura de arquivos. Ideia obter o esquema relacional. Foi seguido o video: https://www.youtube.com/watch?v=C1sx-FEsgOk

---
+++ integracaoDeDados/src/Conversao/Exemplo.java
@@ -0,0 +1,40 @@
+package Conversao;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Exemplo {
+	
+	public static void main(String[] args) {
+		File file = new File("C:/Users/mcf/Documents/IntegracaoDeDados/IntegracaoDeDados/teste.txt");
+		//File file = new File("teste.txt");
+		//File file = new File("C:\Users\mcf\Documents\IntegracaoDeDados\IntegracaoDeDados\teste.txt");
+		
+		try{
+			FileReader reader = new FileReader(file);
+			BufferedReader input = new BufferedReader(reader);
+			String linha;
+			
+			while ((linha = input.readLine()) != null){
+				System.out.println(linha);
+			}
+			
+			input.close();
+		} catch (IOException e){
+			System.out.println(e);
+		}
+		/*try {
+			String arquivo = 
+					  new Scanner(new File("C:/Users/mcf/Documents/IntegracaoDeDados/IntegracaoDeDados/teste.txt"))
+					             .useDelimiter("\\Z").next();
+			System.out.println(arquivo);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
+	}
+}
